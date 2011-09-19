19c463cfe61a2d7d0e78e66d628047d6a5a459ab
John Flanagan <johnflan@gmail.com>
2011-09-19 23:37:20 +0200

    cleaned up the script a little

--- webshots.sh
+++ webshots.sh
@@ -3,17 +3,13 @@
 filename="sites.txt"
 
 while read URL; do
-    dtg=`date +%d_%m_%y_%H%M`
+    DATE=`date +%d_%m_%y_%H%M`
 
     if [ ! -d "$URL" ]; then
         mkdir "$URL"
     fi
 
     OPTIONS="--height 1000 --crop-h 1000 --quality 80"
-    genPDFOfSite="./wkhtmltoimage-amd64 $OPTIONS http://$URL ./$URL.png"
-    $genPDFOfSite
-
-    movecmd="mv ./$URL.png ./$URL/$dtg.png"
-    $movecmd
+    ./wkhtmltoimage-amd64 $OPTIONS http://$URL ./$URL/$DATE.png
 
 done < "$filename"
